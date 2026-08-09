18143e98223e989044522fea6f025a36f1fd84b2
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-08-09 19:09:09 +0800

    Add files via upload

---
+++ assets/useYuEBao-D-a-S2Bg.js
@@ -0,0 +1,1 @@
+import{R as e,r as o,S as s}from"./index-BilwUOjE.js";const d=e("YuEBao",()=>{const t=o(void 0);return{data:s({get:()=>t.value,set:a=>{t.value=a}}),_data:t}},{persist:!0});export{d as u};
